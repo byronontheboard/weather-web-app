edc7cc63ce4206325f708fe4ed40598f75bd37e7
byronallan <127366720+byronallan@users.noreply.github.com>
2023-06-17 06:04:35 -0700

    history is being saved to local and appended to history accordion

--- assets/script.js
+++ assets/script.js
@@ -14,128 +14,182 @@ const weatherBox = document.querySelector('#weather-box');
 const forecastContainer = document.querySelector('.forecast-container');
 const forecastCards = document.querySelectorAll('.forecast-card');
 
+// Array to keep track of saved cities
+const savedCities = [];
+const cityHistory = document.querySelector('#history');
+
+// Load search history from local storage
+let searchHistory = localStorage.getItem('searchHistory');
+if (searchHistory) {
+  const cityHistory = document.querySelector('#history');
+  cityHistory.innerHTML = searchHistory;
+}
+
+// Function to save a searched city to local storage
+function saveSearchHistory(city) {
+  // Check if the city is already saved
+  if (!savedCities.includes(city.toLowerCase())) {
+    savedCities.push(city.toLowerCase());
+    
+  let searchHistory = localStorage.getItem('searchHistory');
+  if (!searchHistory) {
+    searchHistory = '';
+  }
+  searchHistory += `<p>${city}</p>`;
+  localStorage.setItem('searchHistory', searchHistory);
+  } 
+}
+
+
 // Add event listener to the search button
 searchButton.addEventListener('click', () => {
-    const searchTerm = searchInput.value.trim();
-    if (searchTerm !== '') {
-      var apiKey = 'e9ade097c11130cb50bd86df245d32ea';
-      const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}`;
-      const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${apiKey}`;
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm !== '') {
+    var apiKey = 'e9ade097c11130cb50bd86df245d32ea';
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=${apiKey}`;
 
-      // After updating the weather information, this will modify the class of the search box and show the weather box.
-      searchBox.classList.remove('col-12');
-      searchBox.classList.add('col-3');
-      weatherBox.style.display = 'block';
+    // After updating the weather information, this will modify the class of the search box and show the weather box.
+    searchBox.classList.remove('col-12');
+    searchBox.classList.add('col-3');
+    weatherBox.style.display = 'block';
 
-    // Make a request to the OpenWeatherMap API using the search term.
-    fetch(currentWeatherUrl)
-      .then(response => response.json())
-      .then(data => {
-      // Get the weather, wind, and humidity from the response data
-      const weather = data.weather[0].description;
-      // Originally, when a city was searched, the description for the 'Weather' was all lowercase, 
-      // but I wanted it to capitalize the first letter of each word so I made this variable to handle this.
-      const capitalizedWeather = weather
-          // This will split each word in the description for the 'Weather'.
-          .split(' ')
-          // Now that the words are split, I am targeting the first letter of each word('charAt(0)' being the index for the letter) and making it uppercase.
-          // The slice is used to return the rest of the word starting at the second letter('slice(1)' being the index for the second letter and so forth).
-          // This allows only the first letter to be capitalized and not every letter of the word with the help of slice.
-          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-          // Lastly, I join these words back together so that they can be displayed('currentWeatherElement.textContent = `Weather: ${capitalizedWeather}`;').
-          // The reason there is only a space('join(' ')') is because a comma is the default for the separator when using 'join' and I do not desire that output.
-          .join(' ');
-      // This is the variable for the temperature of the city that is searched.  
-      const temperatureKelvin = data.main.temp;
-      // This is using a formula to convert the initial temperature(Kelvin) to Fahrenheit.
-      const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9 / 5 + 32;
-      // This is the default for 'Wind Speed(meters/second)' after retrieving the data.
-      const windMeterSec = data.wind.speed;
-      // I wanted the 'Wind Speed' to be in MPH so I looked up the conversion formula and implemented it into a new variable to display.
-      const windMileHour = windMeterSec * 2.23694; 
-      // This is the variable for the humidity of the city that is searched.
-      const humidity = data.main.humidity;
+  // Make a request to the OpenWeatherMap API using the search term.
+  fetch(currentWeatherUrl)
+    .then(response => response.json())
+    .then(data => {
+    // Get the weather, wind, and humidity from the response data
+    const weather = data.weather[0].description;
+    // Originally, when a city was searched, the description for the 'Weather' was all lowercase, 
+    // but I wanted it to capitalize the first letter of each word so I made this variable to handle this.
+    const capitalizedWeather = weather
+        // This will split each word in the description for the 'Weather'.
+        .split(' ')
+        // Now that the words are split, I am targeting the first letter of each word('charAt(0)' being the index for the letter) and making it uppercase.
+        // The slice is used to return the rest of the word starting at the second letter('slice(1)' being the index for the second letter and so forth).
+        // This allows only the first letter to be capitalized and not every letter of the word with the help of slice.
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        // Lastly, I join these words back together so that they can be displayed('currentWeatherElement.textContent = `Weather: ${capitalizedWeather}`;').
+        // The reason there is only a space('join(' ')') is because a comma is the default for the separator when using 'join' and I do not desire that output.
+        .join(' ');
+    // This is the variable for the temperature of the city that is searched.  
+    const temperatureKelvin = data.main.temp;
+    // This is using a formula to convert the initial temperature(Kelvin) to Fahrenheit.
+    const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9 / 5 + 32;
+    // This is the default for 'Wind Speed(meters/second)' after retrieving the data.
+    const windMeterSec = data.wind.speed;
+    // I wanted the 'Wind Speed' to be in MPH so I looked up the conversion formula and implemented it into a new variable to display.
+    const windMileHour = windMeterSec * 2.23694; 
+    // This is the variable for the humidity of the city that is searched.
+    const humidity = data.main.humidity;
 
-      // Update the corresponding elements with the weather information
-      currentCityElement.textContent = `${searchTerm}`;
-      currentWeatherElement.textContent = `${capitalizedWeather}`;
-      currentTemperatureElement.textContent = `Temperature: ${temperatureFahrenheit.toFixed(1)}°F`;
-      currentWindElement.textContent = `Wind Speed: ${windMileHour.toFixed(1)} MPH`;
-      currentHumidityElement.textContent = `Humidity: ${humidity}%`;
+    // Update the corresponding elements with the weather information
+    currentCityElement.textContent = `${searchTerm}`;
+    currentWeatherElement.textContent = `${capitalizedWeather}`;
+    currentTemperatureElement.textContent = `Temperature: ${temperatureFahrenheit.toFixed(1)}°F`;
+    currentWindElement.textContent = `Wind Speed: ${windMileHour.toFixed(1)} MPH`;
+    currentHumidityElement.textContent = `Humidity: ${humidity}%`;
 
-      // Update the current date element
-      const options = { weekday: 'long', month: 'long', day: 'numeric' };
-      const formattedDate = new Date().toLocaleDateString('en-US', options);
-      currentDateElement.textContent = formattedDate;
-    })
-      .catch(error => {
-          console.log('Error:', error);
-      });
-
-        // Make a request to get the 5-day forecast
-    fetch(forecastUrl)
-      .then(response => response.json())
-      .then(data => {
-        const forecastList = data.list;
-        const fiveDayForecast = forecastList.slice(0, 5);
-
-        fiveDayForecast.forEach((forecast, index) => {
-          const currentDate = new Date();
-          currentDate.setDate(currentDate.getDate() + index + 1);
-          const options = { weekday: 'long', month: 'long', day: 'numeric' };
-          const formattedDate = currentDate.toLocaleDateString('en-US', options);
-          const day = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
-          const weather = forecast.weather[0].description;
-          const capitalizedWeather = weather
-            .split(' ')
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(' ');
-          const temperatureKelvin = forecast.main.temp;
-          const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9 / 5 + 32;
-
-          forecastCards[index].querySelector('.forecast-day').textContent = `${formattedDate}`;
-          forecastCards[index].querySelector('.forecast-weather').textContent = capitalizedWeather;
-          forecastCards[index].querySelector('.forecast-temperature').textContent = `${temperatureFahrenheit.toFixed(1)}°F`;
-        });
-
-        if (fiveDayForecast.length > 0) {
-          forecastContainer.style.display = 'block';
-        } else {
-          forecastContainer.style.display = 'none';
-        }
-      })
-      .catch(error => {
+    // Update the current date element
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    const formattedDate = new Date().toLocaleDateString('en-US', options);
+    currentDateElement.textContent = formattedDate;
+  })
+    .catch(error => {
         console.log('Error:', error);
+    });
+
+      // Make a request to get the 5-day forecast
+  fetch(forecastUrl)
+    .then(response => response.json())
+    .then(data => {
+      const forecastList = data.list;
+      const fiveDayForecast = forecastList.slice(0, 5);
+
+      fiveDayForecast.forEach((forecast, index) => {
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() + index + 1);
+        const options = { weekday: 'long', month: 'long', day: 'numeric' };
+        const formattedDate = currentDate.toLocaleDateString('en-US', options);
+        const day = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
+        const weather = forecast.weather[0].description;
+        const capitalizedWeather = weather
+          .split(' ')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
+        const temperatureKelvin = forecast.main.temp;
+        const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9 / 5 + 32;
+
+        forecastCards[index].querySelector('.forecast-day').textContent = `${formattedDate}`;
+        forecastCards[index].querySelector('.forecast-weather').textContent = capitalizedWeather;
+        forecastCards[index].querySelector('.forecast-temperature').textContent = `${temperatureFahrenheit.toFixed(1)}°F`;
       });
+
+      if (fiveDayForecast.length > 0) {
+        forecastContainer.style.display = 'block';
+      } else {
+        forecastContainer.style.display = 'none';
+      }
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+    
+    // Save searched city to local storage.
+    saveSearchHistory(searchTerm);
+
+    // Check if the city is already saved
+    if (!savedCities.includes(searchTerm.toLowerCase())) {
+      savedCities.push(searchTerm.toLowerCase());
+
+      // Append the search term to the history element
+      const cityElement = document.createElement('p');
+      cityElement.textContent = searchTerm;
+      cityElement.classList.add('history-city');
+      cityHistory.appendChild(cityElement);
+
+      // Clear the search input
+      searchInput.value = '';
     }
+  }
 });
 
 // Add event listener to the search input for autocomplete
 $(searchInput).autocomplete({
-    source: function (request, response) {
-      const searchTerm = request.term.trim();
-      if (searchTerm !== '') {
-        var apiKey = 'e9ade097c11130cb50bd86df245d32ea';
-        var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchTerm},us&limit=10&appid=${apiKey}`;
+  source: function (request, response) {
+    const searchTerm = request.term.trim();
+    if (searchTerm !== '') {
+      var apiKey = 'e9ade097c11130cb50bd86df245d32ea';
+      var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchTerm},us&limit=10&appid=${apiKey}`;
 
-        // Make a request to the OpenWeatherMap API using the search term
-        fetch(apiUrl)
-          .then(response => response.json())
-          .then(data => {
-            const cities = data.map(city => `${city.name}, ${city.state}, US`);
-            response(cities);
-          })
-          .catch(error => {
-            console.log('Error:', error);
-            response([]);
-          });
-      } else {
-        response([]);
-      }
-    },
-    minLength: 2,
-    select: function (event, ui) {
-      searchInput.value = ui.item.value;
-      searchButton.click();
+      // Make a request to the OpenWeatherMap API using the search term
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          const cities = data.map(city => `${city.name}, ${city.state}, US`);
+          response(cities);
+        })
+        .catch(error => {
+          console.log('Error:', error);
+          response([]);
+        });
+    } else {
+      response([]);
     }
+  },
+  minLength: 2,
+  select: function (event, ui) {
+    searchInput.value = ui.item.value;
+    searchButton.click();
+  }
+});
+
+// Add event listener to the history of cities to be able to access/click them.
+historyElement.addEventListener('click', event => {
+  const clickedCity = event.target;
+  if (clickedCity.classList.contains('history-city')) {
+    const searchTerm = clickedCity.textContent;
+    searchInput.value = searchTerm;
+    searchButton.click();
+  }
 });
